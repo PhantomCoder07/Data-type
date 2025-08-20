1d59c789c15c26a695ee27c521da19809d3eb348
Phantom Coder <abdulahad474783@gmail.com>
2025-08-20 16:26:45 +0600

    main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    printf ("Size of Different Data Types:\n\n");
+    printf ("The size of 'int' data type        : %d bytes\n",sizeof(int));
+    printf ("The size of 'float' data type      : %d bytes\n",sizeof(float));
+    printf ("The size of 'char' data type       : %d bytes\n",sizeof(char));
+    printf ("The size of 'double' data type     : %d bytes\n",sizeof(double));
+    printf ("The size of 'long long' data type  : %d bytes\n",sizeof(long long));
+    printf ("The size of 'long double' data type: %d bytes\n",sizeof(long double));
+    return 0;
+}
